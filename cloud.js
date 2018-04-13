0d06e5c122c605432d0e2e2d83e58c8311051fea
Kisn <312339704@qq.com>
2018-04-13 12:23:52 +0800

    周报系统-发送邮件
    周报系统-发送邮件

---
+++ cloud.js
@@ -0,0 +1,145 @@
+let AV = require('leanengine');
+let api = require('./getData.js');
+
+const http = require('http');
+const querystring = require('querystring');
+
+/**
+ * 一个简单的云代码方法
+ */
+AV.Cloud.define('hello', function(request) {
+  return 'Hello world!';
+});
+
+function writeLog(info) {
+  let date = new Date();
+  let Log = AV.Object.extend('EmailLogs');
+  let log = new Log();
+  log.set('date', date);
+  log.set('info', info);
+  log.save().then(res => {
+    console.log('邮件发送日志已记录！');
+  });
+}
+// 发起一个post请求
+function post(data) {
+  const options = {
+    hostname: 'http://192.168.118.59',
+    port: 8066,
+    path: '/weeklyreport/mail/sendmail.php',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      'Content-Length': Buffer.byteLength(data)
+    }
+  };
+
+  const req = http.request(options, res => {
+    console.log(`STATUS: ${res.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+    res.setEncoding('utf8');
+    let info = '';
+    res.on('data', chunk => {
+      info += chunk;
+      // console.log(`BODY: ${chunk}`);
+    });
+    res.on('end', () => {
+      writeLog(info);
+    });
+  });
+
+  req.on('error', e => {
+    console.error(`problem with request: ${e.message}`);
+  });
+
+  // write data to request body
+  req.write(data);
+  req.end();
+}
+
+// 给所有需要填写日志的人发邮件
+AV.Cloud.define('sendEmail', function(request) {
+  console.log('准备处理发送邮件');
+  let d = new Date();
+  console.log(d);
+  return api.getAllUsers().then(result => {
+    let users = [];
+    result.forEach(item => {
+      // 排除不用填写日志的人
+      if (!item.attributes.noReport && item.attributes.email) {
+        users.push({
+          name: item.attributes.username,
+          email: item.attributes.email
+        });
+      }
+    });
+    console.log('=======================');
+    console.log('准备给所有用户发送邮件：');
+    console.log(JSON.stringify(users, 0, 4));
+
+    // 组织数据发请求
+    const data = querystring.stringify({
+      user: JSON.stringify(users),
+      type: 'all'
+    });
+    post(data);
+
+    console.log('发送处理完成, 耗时' + (+new Date() - d) + 'ms');
+    return users;
+  });
+});
+
+// 周六未填再次提醒
+AV.Cloud.define('sendEmailAgain', function(request) {
+  console.log('准备处理发送邮件');
+  let d = new Date();
+  console.log(d);
+  return api.getUnSubmitUsers().then(users => {
+    console.log('=======================');
+    console.log('周六：准备给未提交用户发送邮件：');
+    console.log('未提交用户为:');
+    console.log(JSON.stringify(users, 0, 4));
+
+    post(
+      querystring.stringify({
+        type: 'unsubmit',
+        user: JSON.stringify(users)
+      })
+    );
+
+    console.log('发送处理完成, 耗时' + (+new Date() - d) + 'ms');
+  });
+});
+// 周日警告提醒
+AV.Cloud.define('sendEmailwarning', function(request) {
+  console.log('准备处理发送邮件');
+  let d = new Date();
+  console.log(d);
+  return api.getUnSubmitUsers().then(users => {
+    console.log('=======================');
+    console.log('周日：准备给未提交用户发送邮件：');
+    console.log('未提交用户为:');
+    console.log(JSON.stringify(users, 0, 4));
+
+    post(
+      querystring.stringify({
+        type: 'warning',
+        user: JSON.stringify(users)
+      })
+    );
+
+    console.log('发送处理完成, 耗时' + (+new Date() - d) + 'ms');
+  });
+});
+
+// 接受客户端保存用户信息的请求
+AV.Cloud.define('savePersonData', function(request) {
+  let id = request.params.id;
+  let data = request.params.data;
+  let keys = Object.keys(data);
+  let person = AV.Object.createWithoutData('_User', id);
+  keys.forEach(k => {
+    person.set(k, data[k]);
+  });
+  return person.save();
+});
---
+++ getData.js
@@ -0,0 +1,67 @@
+let AV = require('leanengine');
+let Promise = require('bluebird');
+
+module.exports = {
+  getAllUsers() {
+    return new AV.Query('_User').find();
+  },
+  getCurrWeekReport() {
+    const today = new Date();
+    const day = today.getDay();
+    const date = today.getDate();
+    const isoDay = day === 0 ? 7 : day;
+
+    let startDate = new Date();
+    let endDate = new Date();
+
+    startDate.setDate(date - isoDay + 1);
+    startDate.setHours(0);
+    startDate.setMinutes(0);
+    startDate.setSeconds(0);
+    startDate.setMilliseconds(0);
+
+    endDate.setDate(date + (7 - isoDay));
+    endDate.setHours(23);
+    endDate.setMinutes(59);
+    endDate.setSeconds(59);
+    endDate.setMilliseconds(999);
+
+    console.log('本周开始时间：' + startDate);
+    console.log('本周结束时间：' + endDate);
+
+    let q = new AV.Query('Logs');
+
+    q.greaterThanOrEqualTo('updatedAt', startDate);
+    q.lessThanOrEqualTo('updatedAt', endDate);
+
+    return q.find();
+  },
+  getUnSubmitUsers() {
+    return Promise.all([this.getAllUsers(), this.getCurrWeekReport()]).then(
+      results => {
+        let users = results[0];
+        let reports = results[1];
+        let submitUser = {};
+        let unsubmitUsers = [];
+        reports.forEach(item => {
+          submitUser[item.attributes.userId] = true;
+        });
+
+        users.forEach(user => {
+          if (
+            !submitUser[user.id] &&
+            !user.attributes.noReport &&
+            user.attributes.email
+          ) {
+            unsubmitUsers.push({
+              name: user.username,
+              email: user.attributes.email
+            });
+          }
+        });
+
+        return unsubmitUsers;
+      }
+    );
+  }
+};
